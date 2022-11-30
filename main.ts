9a5bfe01868bd78f18c5f882e809d0de7b509213
pc20211030 <115674660+pc20211030@users.noreply.github.com>
2022-11-30 19:41:24 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+let x_value = 0
+let y_value = 0
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        led.plot(x_value, 0)
+        x_value += 1
+        basic.pause(200)
+        if (x_value > 4) {
+            x_value = 0
+        }
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        basic.clearScreen()
+        led.plot(x_value - 1, y_value)
+        y_value += 1
+        basic.pause(200)
+        if (4 < y_value) {
+            y_value = 0
+        }
+    }
+})
